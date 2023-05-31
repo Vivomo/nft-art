63fd8c34843cd10b2d069482dd683bdf28853153
vivomo <929992114@qq.com>
2023-06-01 02:47:47 +0800

    ipfs service

---
+++ src/service/ipfs-service.ts
@@ -0,0 +1,28 @@
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { IPFS } from "../config";
+import axios from 'axios'
+const ipfs =  ipfsHttpClient({
+    host: IPFS.domain,
+    port: 5001,
+    protocol: 'http'
+
+})
+export const storeMeta = async (data:any) => {
+
+    const json = JSON.stringify(data);
+    alert(json);
+    try {
+        const added = await ipfs.add(json);
+        alert(added.path)
+    }catch (error) {
+        alert(error);
+    }
+
+}
+export const addToIpfs = async (entity:any) : Promise<string> => {
+    debugger
+    const added = await ipfs.add(entity)
+    const cid = added.path
+    const rst = IPFS.url_prefix + cid;
+    return rst;
+}
